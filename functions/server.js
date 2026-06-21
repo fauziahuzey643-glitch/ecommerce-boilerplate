5f532432369a69d3c4ba633dd73150cf4d37f390
Jodialba15 <fauziahuzey643@gmail.com>
2026-06-22 05:37:18 +0700

    fix: pastikan file jembatan functions lengkap

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../server'); // Mengambil aplikasi Express utama Anda
+const app = require('../server'); // Mengambil aplikasi Express utama Anda di luar
 
 module.exports.handler = serverless(app);
